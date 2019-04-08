e6215419c72f2ae3a67930ef542570982a1e6686
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-04-08 11:11:59 +0600

    Merge with Jaowat

--- src/app/Services/student/home-api.service.ts
+++ src/app/Services/student/home-api.service.ts
@@ -10,6 +10,6 @@ export class HomeApiService {
   constructor(public http: HttpClient) { }
 
   public schedule(): Observable<any> {
-    return this.http.get<any>("http://172.16.22.101:1374/api/Home/Schedule?fromDateTime=2019-04-03 10:11:31.410&tillDateTime=2019-04-10 10:11:31.410");
+    return this.http.get<any>("http://172.16.22.101:4962/api/Home/Schedule?fromDateTime=2019-04-03 10:11:31.410&tillDateTime=2019-04-10 10:11:31.410");
   }
 }

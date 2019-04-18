fd86f1805b5b9a6e62348e01bad450912b2bea33
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-04-18 11:08:44 +0600

    Rxjs Error Handler Added

--- src/app/Services/student/profile-api.service.ts
+++ src/app/Services/student/profile-api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import { IResult } from 'src/app/Core/result/result';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,11 @@ export class ProfileApiService {
   private baseUrl = environment.baseUrl;
 
   public getStudentProfile(): Observable<IResult> {
-    return this.httpClient.get<IResult>(this.baseUrl + "/Home/GetStudentProfile");
+    return this.httpClient.get<IResult>(this.baseUrl + "/Home/GetStudentProfil")
+                          .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error.message || "Server Error");
   }
 }

cd0cf98f7274505dc0d55be1be9d7eb3776067e6
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-05-02 15:21:57 +0600

    Employee home oneSignal

--- src/app/Pages/employee/employee-home/employee-home.page.ts
+++ src/app/Pages/employee/employee-home/employee-home.page.ts
@@ -1,3 +1,4 @@
+import { PushNotificationService } from 'src/app/Core/oneSignal/push-notification.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeHomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pushNotification: PushNotificationService
+    ) { }
 
   ngOnInit() {
+    this.pushNotification.getPlayerID();
   }
 
 }

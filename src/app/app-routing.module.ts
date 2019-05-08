e8c9bda495acce6d0c34e9b7e3f754448f063179
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-05-08 11:30:54 +0600

    Nayan C

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,19 +13,16 @@ const routes: Routes = [
   { path: 'login', loadChildren: './Pages/login/login.module#LoginPageModule' },
   { path: 'logout', loadChildren: './Pages/logout/logout.module#LogoutPageModule' },
 
-
   //STUDENT
   { path: 'home', loadChildren: './Pages/student/home/home.module#HomePageModule' },
   { path: 'profile', loadChildren: './Pages/student/profile/profile.module#ProfilePageModule' },
   { path: 'courses-results', loadChildren: './Pages/student/courses-results/courses-results.module#CoursesResultsPageModule' },
   { path: 'registration', loadChildren: './Pages/student/registration/registration.module#RegistrationPageModule' },
 
-
   //EMPLOYEE
   { path: 'employee-home', loadChildren: './Pages/employee/employee-home/employee-home.module#EmployeeHomePageModule' },
-  { path: 'employee-profile', loadChildren: './Pages/employee/employee-profile/employee-profile.module#EmployeeProfilePageModule' }
-
-
+  { path: 'employee-profile', loadChildren: './Pages/employee/employee-profile/employee-profile.module#EmployeeProfilePageModule' },
+  { path: 'error-landing', loadChildren: './Core/errorLandingPage/error-landing/error-landing.module#ErrorLandingPageModule' }
   
 ];
 

50c07fc7106c21e8daaccd041515d4a28c80d41f
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-05-14 12:13:46 +0600

    Finalcial Page Uodated

--- src/app/Pages/student/financials/financials.page.ts
+++ src/app/Pages/student/financials/financials.page.ts
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
 import { LoadingService } from 'src/app/core/loader/loading.service';
-=======
->>>>>>> Stashed changes
 import { FinancialService } from './../../../Services/student/financial.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -13,24 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class FinancialsPage implements OnInit {
 
   financialDeatils: any;
-<<<<<<< Updated upstream
   totalBalance: number;
 
   constructor(
     private financialService : FinancialService,
     private loadingService: LoadingService
     ) { }
-=======
 
-  constructor(private financialService : FinancialService) { }
->>>>>>> Stashed changes
-  
   ngOnInit() {
     this.getFinancialDetails();
   }
 
   getFinancialDetails(){
-<<<<<<< Updated upstream
     this.loadingService.loadingStart();
     this.financialService.financialDetails().subscribe(res => {
       this.loadingService.loadingDismiss();
@@ -42,11 +33,6 @@ export class FinancialsPage implements OnInit {
   calculateTotalBalance (debit: number, credit: number): number {
     let result = debit - credit;
     return Number(result.toFixed(2));
-=======
-    this.financialService.financialDetails().subscribe(res=>{
-      this.financialDeatils = res.Data;
-    })
->>>>>>> Stashed changes
   }
 
 }

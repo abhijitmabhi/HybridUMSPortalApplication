21696b731113929a40ec7ff79c3bc750672d24dd
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-05-15 15:56:13 +0600

    Registration Code Modified

--- src/app/Pages/student/registration/registration.page.ts
+++ src/app/Pages/student/registration/registration.page.ts
@@ -3,11 +3,6 @@ import { RegistrationService } from './../../../Services/student/registration.se
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/Services/common/common.service';
 
-export interface SelectSemester {
-  value: any;
-  viewValue: any;
-}
-
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
@@ -24,48 +19,38 @@ export class RegistrationPage implements OnInit {
     private registrationService: RegistrationService,
     private loadingService: LoadingService) { }
 
-  semesters: SelectSemester[] = [
-    {value: '2017-18, Spring', viewValue: '2017-18, Spring'},
-    {value: '2017-18, Summer', viewValue: '2017-18, Summer'},
-    {value: '2018-19, Fall', viewValue: '2018-19, Fall'},
-    {value: '2017-18, Spring', viewValue: '2018-19, Spring'},
-    {value: '2017-18, Summer', viewValue: '2018-19, Summer'}
-  ];
-  
   ngOnInit() {
     this.getSemesterList();
   }
 
-  getSemesterList(){
-    this.loadingService.loadingStart();
+  getSemesterList() {
     this.commonService.semesterList().subscribe(semesterLists => {
       this.semesterList = semesterLists.Data;
       this.commonService.currentSemester().subscribe(currentSemester => {
-        this.loadingService.loadingDismiss();
         let currentSemesterId = currentSemester.Data.ID;
         let isCurrentSemesterEnrolled = false;
         this.semesterList.forEach(semester => {
-          if(semester.ID === currentSemesterId){
+          if (semester.ID === currentSemesterId) {
             isCurrentSemesterEnrolled = true;
           }
         });
 
-        if(isCurrentSemesterEnrolled){
+        if (isCurrentSemesterEnrolled) {
           this.nrSelect = currentSemesterId;
-        }else {
+        } else {
           this.nrSelect = this.semesterList[0].ID;
         }
       })
     })
   }
 
-  onChangeSemester(){
+  onChangeSemester() {
     this.loadingService.loadingStart();
     this.registrationService.getRegistrationDetails(this.nrSelect).subscribe(res => {
       this.loadingService.loadingDismiss();
       this.registrationData = res.Data;
     })
   }
-  
+
 
 }

0113a445850d18ecefd21a26eefd94dd884c767e
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2019-07-08 10:57:43 +0600

    Recent Commit

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -37,8 +37,8 @@ export class LoginPage implements OnInit {
     this.User = UserModel;
     this.Cred = CredModel;
 
-    // this.User.password = '243866';
-    // this.User.username = '16-31332-1';
+    this.User.password = '243866';
+    this.User.username = '16-31332-1';
     // this.User.password = '26103588';
     // this.User.username = '1801-1848-3';
     // this.User.password = '58389796';
